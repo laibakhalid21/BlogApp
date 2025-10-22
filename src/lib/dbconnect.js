610a02cfdcf4fca5f788504259271f20af1b18e4
coding-practice <laibakhalid2109@gmail.com>
2025-10-23 01:03:15 +0500

    fix

--- src/lib/dbconnect.js
+++ src/lib/dbconnect.js
@@ -8,7 +8,7 @@ const DbConnect=async()=>{
         }
     try {
        
-       const db=await mongoose.connect(process.env.MONGODB_URL)
+       const db=await mongoose.connect(process.env.MONGODB_URI)
        connection.isConnected=db.connections[0].readyState;
        console.log("DB connected successfully");
     } catch (error) {
