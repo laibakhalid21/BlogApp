8a4e314cafdcd94b8434ee7115e160f6f6dae849
coding-practice <laibakhalid2109@gmail.com>
2025-10-23 00:26:41 +0500

    fix

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
-export const revalidate = 0;
+// export const fetchCache = "force-no-store";
+// export const revalidate = 0;
 
 import React, { useEffect, useState, Suspense } from "react";
 import { signIn, useSession } from "next-auth/react";
